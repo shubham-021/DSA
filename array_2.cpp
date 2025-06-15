3115ccb72c99a466b95e0f23528c59450defce54
shubham-021 <shubham.arka@gmail.com>
2025-06-16 01:41:21 +0530

    dutch it

--- array_2.cpp
+++ array_2.cpp
@@ -95,9 +95,155 @@ int longest_subarray_sum(vector<int> arr , int k){
     return maxi;
 }
 
+int optimal_longest_subarray(vector<int> arr , int k){
+    int maxi = 0;
+    int size = arr.size();
+    int cnt;
+    int sum = 0;
+    int i = 0;
+    int j = 0;
+
+    while(j<size){
+        sum += arr[j];
+        if(sum==k){
+            cnt = j-i+1;
+            maxi = max(cnt,maxi);
+        }else if(sum > k){
+            sum -= arr[i];
+            i++;
+        }
+        j++;
+    }
+    return maxi;
+}
+
+
+// array of digits , add one and return the number
+
+void plus_one(vector<int> &arr){
+    int carry = 1;
+    int size = arr.size();
+    int i = size-1;
+    while(i>0){
+        int plus = arr[i] + carry;
+        if(plus % 10 == 0){
+            arr[i] = 0;
+        }else{
+            arr[i] = plus;
+            break;
+        }
+        i--;
+    }
+    if(i==0&&arr[i]==9)arr[i] = 10;
+}
+
+// two sum
+
+// returning bool value for whether two sum exist or not
+bool two_sum(vector<int> arr , int sum){
+    bool result = false;
+    map<int , int> mp;
+    for(auto it:arr){
+        int diff = sum - it;
+        mp[it]++;
+        if(it != diff) mp[diff]++;
+    }
+
+    for(auto it : mp){
+        if(it.second > 1){
+            result = true;
+            break;
+        }
+    }
+
+    return result;
+}
+
+//returning indices if two sum exists
+// vector<int> two_sum_indices(vector<int> arr , int sum){
+//     vector<int> indicies;
+//     map<int , int> mp;
+//     for(int i = 0; i < arr.size(); i++){
+//         mp[arr[i]] = i;
+//     }
+//     for(int i = 0 ; i < arr.size() ; i++){
+//         int diff = sum - arr[i];
+//         if(mp.find(diff) != mp.end()){
+//             int index = mp[diff];
+//             if(index != i){
+//                 indicies.push_back(i);
+//                 indicies.push_back(index);
+//                 break;
+//             }
+//         }
+//     }
+//     return indicies;
+// }
+
+//better way to write the same
+vector<int> two_sum_indices(vector<int> arr , int sum){
+    vector<int> indicies;
+    map<int , int> mp;
+    for(int i = 0; i < arr.size(); i++){
+        int diff = sum - arr[i];
+        if(mp.find(diff) == mp.end()){
+            mp[arr[i]] = i;
+        }else{
+            indicies.push_back(mp[diff]);
+            indicies.push_back(i);
+            break;
+        }
+    }
+    return indicies;
+}
+
+// two pointer approach
+
+string two_sum_pointer(vector<int> &arr , int target){
+    int n = arr.size();
+    int left = 0;
+    int right = n-1;
+    sort(arr.begin(),arr.end());
+    while(left<right){
+        int sum = arr[left]+arr[right];
+        if(sum==target) return "YES";
+        if(sum<target){
+            left++;
+        }else{
+            right--;
+        }
+    }
+    return "NO";
+}
+
+// sorting array containing 0,1,2
+// brute force way will be to use any sorting algo
+// better will be to count 0,1,2 and store them in a varibale , then run loops for all three from 0 to 2 , to replace the elements with the respective digit
+// optimal way will be to use dutch national flag algo , use three pointer approach
+
+void dutch(vector<int> &arr){
+    int n = arr.size();
+    int low = 0;
+    int mid = 0;
+    int high = n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+        }else if(arr[mid]==1){
+            mid++;
+        }else if(arr[mid]==2){
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+}
+
 
 int main(){
-    vector<int> arr = {15, 5, 2, 7, 9};
-    cout<<longest_subarray_sum(arr,15);
+    vector<int> arr = {2,0,1}; // --> {1,0,2}
+    dutch(arr);
+    for(auto it:arr)cout<<it<<" ";
     return 0;
 }
